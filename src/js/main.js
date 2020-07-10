09763edaa1c0d82f730001be96b0aba0dcaf46d4
Kaoy <howkiens@gmail.com>
2020-07-10 15:37:02 +0300

    header done

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
-
   $(".toggle-menu").click(function (event) {
     event.preventDefault();
-    $(".header").toggleClass("active");
+    $("body").toggleClass("active");
     $(".toggle-menu").toggleClass("active");
-    // $(".navigation__menu").toggleClass("active");
-
+    $(".navigation__menu").toggleClass("active");
   });
 
-  $(".navigation__nav-item").click(function (event) {
-    $(".header").removeClass("active");
+  $(".menu-list__item").click(function (event) {
+    $("body").removeClass("active");
     $(".toggle-menu").removeClass("active");
-    // $(".navigation__menu").removeClass("active");
+    $(".navigation__menu").removeClass("active");
+  });
+
+  window.addEventListener("resize", function () {
+    $("body").removeClass("active");
+    $(".toggle-menu").removeClass("active");
+    $(".navigation__menu").removeClass("active");
   });
 });
